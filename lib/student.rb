22fa4f20053c1256ebd008cb6da60bcb8397e3f6
Micah Banschick <micha.ban@gmail.com>
2020-03-23 18:13:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,4 +40,21 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.new_from_db
+  end 
+  
+  def self.find_by_name(name)
+  end 
+  
+  def self.all_students_in_grade_9
+  end 
+  
+  def self.students_below_12th_grade
+  end 
+  
+  def self.all 
+  end 
+  
+  def self.
 end
