2ac55de03b9254b58ddeae58fac8413f627f812e
Micah Banschick <micha.ban@gmail.com>
2020-03-23 18:48:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,7 +84,7 @@ class Student
   def self.first_student_in_grade_10
     sql = <<-SQL 
       SELECT * FROM students 
-      WHERE grade = 10
+      WHERE grade = 10 ORDER BY id
       LIMIT 1
     SQL
     DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
