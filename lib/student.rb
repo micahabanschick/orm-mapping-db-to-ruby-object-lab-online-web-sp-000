cd9b6ba9cf7d06c35803b858361eab2b8ac7b87d
Micah Banschick <micha.ban@gmail.com>
2020-03-23 18:47:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,8 +82,19 @@ class Student
   end 
   
   def self.first_student_in_grade_10
+    sql = <<-SQL 
+      SELECT * FROM students 
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
   end 
   
-  def self.all_students_in_grade_X 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL 
+      SELECT * FROM students 
+      WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, grade).map{|row| self.new_from_db(row)}
   end 
 end
